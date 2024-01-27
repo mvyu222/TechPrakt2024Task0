110fcdfd739528b8138a6e3fc886275ae6305394
Vadym Mykolaichuk <ipz222_mvyu@student.ztu.edu.ua>
2024-01-27 17:23:45 +0200

    project

---
+++ pract/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine(sum(1,2));
+
+    }
+
+    private static int sum(int number1,int number2)
+    {
+        return number1 + number2;
+    }
+
+
+
+}
